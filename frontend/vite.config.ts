9c0fbfefdfed4b8b8914b13b35326d4cfef1c9ff
Eduardo Marques <eedumarquess@gmail.com>
2026-03-16 16:32:29 -0300

    chore(frontend): add Vite config

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  envDir: '..',
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+  },
+});
