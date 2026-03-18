c0f77996cf0384e53e7ad3c4b5e473b4ea52f89d
eedumarquess <eedumarquess@gmail.com>
2026-03-18 10:17:30 -0300

    feat(delivery-persons): add delivery person http and persistence layer

--- backend/src/delivery-persons/delivery-persons.module.ts
+++ backend/src/delivery-persons/delivery-persons.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DeliveryPersonsController } from './delivery-persons.controller';
+import { DeliveryPersonsRepository } from './delivery-persons.repository';
 import { DeliveryPersonsService } from './delivery-persons.service';
 
 @Module({
-  providers: [DeliveryPersonsService],
+  controllers: [DeliveryPersonsController],
+  providers: [DeliveryPersonsRepository, DeliveryPersonsService],
   exports: [DeliveryPersonsService],
 })
 export class DeliveryPersonsModule {}
