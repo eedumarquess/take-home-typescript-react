1953b7f135b603fc04d81c8281d1ae823fe82cbf
eedumarquess <eedumarquess@gmail.com>
2026-03-18 16:31:09 -0300

    chore(ddd): migrate batch 25

--- backend/src/orders/dto/assign-delivery-person.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsUUID } from 'class-validator';
-
-export class AssignDeliveryPersonDto {
-  @IsUUID('4', { message: 'O entregador informado e invalido' })
-  deliveryPersonId!: string;
-}
